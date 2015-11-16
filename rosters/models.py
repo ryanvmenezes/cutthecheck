13936592e5b677e3dd25c0f607c970829bd0f664
ryanvmenezes <ryanvmenezes@gmail.com>
2015-11-16 13:54:53 -0800

    an attempt at relationships

--- rosters/models.py
+++ rosters/models.py
@@ -3,44 +3,45 @@ from django.db import models
 # Create your models here.
 
 class Player(models.model):
-	full_name=models.CharField(
+	full_name = models.CharField(
         max_length=150,
         null=True,
         help_text="NBA player's name",
     )
-	nba_team=models.CharField(
+	nba_team = models.CharField(
         max_length=150,
         null=True,
         help_text="NBA player's team",
     )
-	salary_1516=models.IntegerField(
+	salary_1516 = models.IntegerField(
 		help_text="Cap hit for this player"
 	)
-	MANAGER_CHOICES = (
-		('Austin','Austin',),
-		('Sam','Sam',),
-		('Eli','Eli',),
-		('Pedro','Pedro',),
-		('Zach','Zach',),
-		('Everett','Everett',),
-		('Kartje','Kartje',),
-		('Menezes','Menezes',),
-		('Justin','Justin',),
-		('Vineeth','Vineeth',),
-		(None, "FA")
-	)
-	manager=models.CharField(
-        max_length=150,
-        null=False,
-        help_text="Who owns this player",
-        choices=MANAGER_CHOICES,
-    )
+	manager = models.OneToOneField(Squad)
 
 class Squad(models.model):
-	manager=models.CharField(
+    MANAGER_CHOICES = (
+        ('Austin','Austin',),
+        ('Sam','Sam',),
+        ('Eli','Eli',),
+        ('Pedro','Pedro',),
+        ('Zach','Zach',),
+        ('Everett','Everett',),
+        ('Kartje','Kartje',),
+        ('Menezes','Menezes',),
+        ('Justin','Justin',),
+        ('Vineeth','Vineeth',),
+        # (None, "FA")
+    )
+	manager = models.CharField(
         max_length=150,
         null=True,
         help_text="Who helms this ship",
         choices=MANAGER_CHOICES,
     )
-	player=models.ForeignKey(Player)
+    player = models.ForeignKey(Player)
+
+    @property
+    def total_cap_hit(self):
+        total = 0
+        for p in self.player_set.all():
+            total += p.salary_1516
